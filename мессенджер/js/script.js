783af03c9bd52917f69213bd52054145c17e9e28
3-126-8-PC <you@example.com>
2025-05-14 12:57:31 +1200

    14.05.25

--- мессенджер/js/script.js
+++ мессенджер/js/script.js
@@ -2,70 +2,110 @@ const host = `http://api-messenger.web-srv.local`
 const content = document.querySelector(".content")
 
 
-function _post (params, callback) {
+function _post(params, callback) {
     let http_request = new XMLHttpRequest();
     http_request.open('POST', `${params.url}`)
     http_request.send(params.data)
     http_request.onreadystatechange = function () {
-        if(http_request.readyState ==4){
+        if (http_request.readyState == 4) {
             callback(http_request.responseText)
         }
     }
 }
 
-function _get (params, callback) {
+function _get(params, callback) {
     let http_request = new XMLHttpRequest();
-    http_request.open('GET',`${params.url}`);
+    http_request.open('GET', `${params.url}`);
     http_request.send();
-    http_request.onreadystatechange = function (){
-        if (http_request.readyState == 4){
+    http_request.onreadystatechange = function () {
+        if (http_request.readyState == 4) {
             callback(http_request.responseText)
         }
     };
 }
-/*
-_post({url:'/modules/registr.html'}, function(response){
+
+_post({ url: '/modules/chat.html' }, function (response) {
     content.innerHTML = response;
     LoadPageChats()
+    LoadPageAuth()
 })
 
-function _elem(selector){
+function _elem(selector) {
     return document.querySelector(selector)
 }
 
 /*Регистрация*/
-/*function LoadPageChats() {
-    _elem('.registr').addEventListener('click',function(){
-    
+function LoadPageChats() {
+    _elem('.registr').addEventListener('click', function () {
 
 
-    let rdata = new FormData()
-    let first_name = _elem('input[name="first_name"]').value
-     let last_name = _elem('input[name="last_name"]').value
-      let sur_name = _elem('input[name="sur_name"]').value
-       let email = _elem('input[name="email"]').value
+
+        let rdata = new FormData()
+        let first_name = _elem('input[name="first_name"]').value
+        let last_name = _elem('input[name="last_name"]').value
+        let sur_name = _elem('input[name="sur_name"]').value
+        let email = _elem('input[name="email"]').value
         let password = _elem('input[name="password"]').value
-        rdata.append('first_name',first_name)
-         rdata.append('last_name',last_name)
-          rdata.append('sur_name',sur_name)
-           rdata.append('email',email)
-            rdata.append('password',password)
+        rdata.append('name', first_name)
+        rdata.append('fam', last_name)
+        rdata.append('otch', sur_name)
+        rdata.append('email', email)
+        rdata.append('pass', password)
 
-            _post({url:`${host}/user/`, data: rdata}, function(response){
-                response = JSON.parse(response)
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', `${host}/user/`);
+        xhr.send(rdata);
+        xhr.onreadystatechange = function () {
+            if (xhr.status == 200) {
+                OnLoadPageChats()
+            } if (xhr.status == 422) {
+                let response = JSON.parse(xhr.responseText)
+                alert(response.message)
+            }
+        }
+    })
 
-                if (response.success) {
-                    token = response.token
-                    console.log(token)
+}
 
-                }
-            })
-})
+function OnLoadPageChats() {
+    _post({ url: '/modules/chat.html' }, function (response) {
+        content.innerHTML = response;
+    })
+}
 
-}*/
+function LoadPageAuth() {
+    document.querySelector('.authorize').addEventListener('click', function() {
+        _post({ url: '/modules/author.html' }, function (response) {
+        content.innerHTML = response;
+        LoadPageChatsAuth()
+    })
+    })
+}
 
-   
-_post({url:'/modules/chat.html'}, function(response){
-    content.innerHTML = response;
-    
-})
+
+function LoadPageChatsAuth() {
+    _elem('.author').addEventListener('click', function () {
+
+        let edata = new FormData()
+        let email = _elem('input[name="email"]').value
+        let password = _elem('input[name="password"]').value
+        edata.append('email', email)
+        edata.append('pass', password)
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', `${host}/auth/`);
+        xhr.send(edata);
+        xhr.onreadystatechange = function () {
+            if (xhr.status == 200) {
+                OnLoadPageChats()
+            } if (xhr.status == 401) {
+                let response = JSON.parse(xhr.responseText)
+                alert(response.message)
+            }
+        }
+    })
+
+}
+
+
+
